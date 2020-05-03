bea4ecf4ec55827f490152c12de6c5d969e40e62
Claire Kodia <claire.kodia@gmail.com>
2020-05-03 22:22:39 +0200

    Updates

---
+++ precache-manifest.10059902b19b7f0290d88529dacf5b63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4a329660f9d5bdb7397ce6e758f6e9c",
+    "url": "/travel_memory_card/index.html"
+  },
+  {
+    "revision": "dd4a8489130671cceb9a",
+    "url": "/travel_memory_card/static/css/main.13896cc1.chunk.css"
+  },
+  {
+    "revision": "19f7bacc602b6fcc6d30",
+    "url": "/travel_memory_card/static/js/2.cbf59ec0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/travel_memory_card/static/js/2.cbf59ec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd4a8489130671cceb9a",
+    "url": "/travel_memory_card/static/js/main.cf42260e.chunk.js"
+  },
+  {
+    "revision": "0bc374cfae06c796bf74",
+    "url": "/travel_memory_card/static/js/runtime-main.d916ffa1.js"
+  }
+]);
